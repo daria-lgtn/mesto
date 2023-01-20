43be4c793a215abcd56e0debdaa29a6d15ac4936
daria-lgtn <mikhailchukhnin@mikhails-MacBook-Pro.local>
2023-01-20 11:47:17 +0300

    fix: changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,44 @@
-const popupOpenButton = document.querySelector(".traveller__info-full-name-edit-btn");
+const popupOpenButton = document.querySelector(
+  ".traveller__info-full-name-edit-btn"
+);
 const popupCloseButton = document.querySelector(".popup__container-close-btn");
 
 const popupContainer = document.querySelector(".popup");
 const popupForm = document.querySelector(".popup__container-form");
-const popupInputName = document.querySelector(".popup__container-input_type_name");
-const popupInputDescription = document.querySelector(".popup__container-input_type_description");
+const popupInputName = document.querySelector(
+  ".popup__container-input_type_name"
+);
+const popupInputDescription = document.querySelector(
+  ".popup__container-input_type_description"
+);
 
-const pageInputName = document.querySelector(".traveller__info-full-name-label");
-const pageInputDescription = document.querySelector(".traveller__info-description");
+const pageInputName = document.querySelector(
+  ".traveller__info-full-name-label"
+);
+const pageInputDescription = document.querySelector(
+  ".traveller__info-description"
+);
 
-function popupClose () {
-    popupContainer.classList.remove("popup_opened");
+function popupCloseHandler() {
+  popupContainer.classList.remove("popup_opened");
 }
 
-function popupOpen () {
-    popupContainer.classList.add("popup_opened");
+function popupOpenHandler() {
+  popupContainer.classList.add("popup_opened");
 
-    popupInputName.value = pageInputName.textContent.trim();
-    popupInputDescription.value = pageInputDescription.textContent.trim();
+  popupInputName.value = pageInputName.textContent.trim();
+  popupInputDescription.value = pageInputDescription.textContent.trim();
 }
 
-popupCloseButton.addEventListener("click", popupClose);
-popupOpenButton.addEventListener("click", popupOpen);
+function popupSubmitHandler(event) {
+  event.preventDefault();
 
-function popupSubmit(event) {
-    event.preventDefault();
+  pageInputName.textContent = popupInputName.value;
+  pageInputDescription.textContent = popupInputDescription.value;
 
-    pageInputName.textContent = popupInputName.value;
-    pageInputDescription.textContent = popupInputDescription.value;
-
-    popupClose()
+  popupCloseHandler();
 }
 
-popupForm.addEventListener('submit', popupSubmit);
+popupCloseButton.addEventListener("click", popupCloseHandler);
+popupOpenButton.addEventListener("click", popupOpenHandler);
+popupForm.addEventListener("submit", popupSubmitHandler);
